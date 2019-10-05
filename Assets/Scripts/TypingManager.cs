e2583f4d5386daec725f94b0bba711ec8ad88a68
Jonah Venglarcik <jonahvenglarcik@gmail.com>
2019-10-05 14:51:19 -0700

    Generalize TypingManager

--- Assets/Scripts/TypingManager.cs
+++ Assets/Scripts/TypingManager.cs
@@ -6,13 +6,15 @@ using UnityEngine.Events;
 
 public class TypingManager : MonoBehaviour
 {
-    public List<Line> lines;
-    public Text display;
+    public Line line;
     public Text cursor;
+    public Text toType;
+    public Text userInput;
 
     void Start()
     {
-        lines.Add(new Line("void Start()"));
+        line = new Line("void Start()");
+        toType.text = line.text;
     }
 
     // Update is called once per frame
@@ -29,22 +31,17 @@ public class TypingManager : MonoBehaviour
         // If we are typing, check typed character
         string typing = "";
         char c = input[0];
-        for (int i = 0; i < lines.Count; i++)
+        string typed = line.continueText(c);
+        typing += typed + "\n";
+
+        // If we typed the whole word
+        if (typed.Equals(line.text))
         {
-            Line line = lines[i];
-            string typed = line.continueText(c);
-            typing += typed + "\n";
-            // If we typed the whole word
-            if (typed.Equals(line.text))
-            {
-                // Remove word from list
-                Debug.Log("TYPED : " + line.text);
-                lines.Remove(line);
-                break;
-            }
-            
+            Debug.Log("TYPED : " + line.text);
         }
-        display.text = typing;
+            
+        
+        userInput.text = typing;
         cursor.text = "";
         for (int i = 1; i < typing.Length; i++)
         {
