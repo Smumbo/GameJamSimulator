14985bdfad000fd6e4414debd75fa49b1b6ef882
Jade <jhmarmo@charter.net>
2019-10-05 14:09:12 -0700

    mouse drag
    have mouse drag working, yeehaw

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Collider2D collider;
+    private float deltaX;
+    private float deltaY;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnMouseDown()
+    {
+        deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+        deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 mousePostion = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(mousePostion.x - deltaX, mousePostion.y - deltaY);
     }
 }
